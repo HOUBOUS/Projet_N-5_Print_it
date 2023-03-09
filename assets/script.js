31e24fbf88f1e34df940c427b1c10c5331197752
Houda Bouslama <houda.bouslama81@hotmail.com>
2023-03-09 11:03:04 +0100

    Ajout des eventlisteners

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,38 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+/*Ajout des eventListeners sur les fléches*/
+
+//Cette fonction afficher le slide//
+ function hideSlide (){
+
+ }
+
+//Cette fonction cacher le slide node.removeChild(child); child est le nœud enfant à retirer du DOM.node est le nœud parent de child.//
+function hideSlide (slides, img){
+	banner.removeChild(document.queryselector('.banner-img')); //J'ai supprimé la class banner-image//
+	banner.removeChild(document.queryselector('p')) //J'ai supprimé la balise p//
+ }
+
+ function displaySlide (slides, img){
+	let img = document.getElementById('.banner-img'); //J'ai supprimé la class banner-image//
+	let text= document.queryselector('p') //J'ai supprimé la balise p//
+ }
+
+
+Let arrowRight = document.querySelector(".arrow_right");
+
+arrowRight.addEventListener('clik', fonction(){
+       hideSlide();
+	   displaySlide()
+
+})
+
+Let arrowLeft = document.querySelector(".arrow_left");
+
+arrowRight.addEventListener('clik', fonction(){
+       hideSlide();
+	   displaySlide()
+
+})
